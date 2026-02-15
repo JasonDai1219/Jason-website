30a8fb0cbc0255727a1032f5741414605f7562fa
Jason(Ruizhe) Dai <114436475+JasonDai1219@users.noreply.github.com>
2026-02-14 22:55:36 -0500

    Fix types and project structure for deployment

--- config/socials.ts
+++ config/socials.ts
@@ -12,18 +12,18 @@ export const SocialLinks: SocialInterface[] = [
     name: "GitHub",
     username: "ruizhedai",
     icon: Icons.gitHub,
-    link: "https://github.com/ruizhedai",
+    link: "https://github.com/JasonDai1219",
   },
   {
     name: "LinkedIn",
     username: "Ruizhe Dai",
     icon: Icons.linkedin,
-    link: "https://www.linkedin.com/in/ruizhe-dai/",
+    link: "https://www.linkedin.com/in/jason-dai9/",
   },
   {
     name: "Email",
-    username: "ruizhe.dai@seas.upenn.edu",
+    username: "dai9@seas.upenn.edu",
     icon: Icons.gmail,
-    link: "mailto:ruizhe.dai@seas.upenn.edu",
+    link: "mailto:dai9@seas.upenn.edu",
   },
 ];
